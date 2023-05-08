7515f67bf52b810bdd910d376973d10510576c9f
GodBiggestL <94173947+GodBiggestL@users.noreply.github.com>
2023-05-08 16:26:56 +0530

    Add files via upload

---
+++ Team-Voting-main/config.js
@@ -0,0 +1,23 @@
+import firebase from 'firebase';
+
+// add SDK Firebase
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC6hf1b3ffxZP__CwZyrqT57rhmG4mXSx8",
+  authDomain: "team-voter-3.firebaseapp.com",
+  projectId: "team-voter-3",
+  storageBucket: "team-voter-3.appspot.com",
+  messagingSenderId: "267641714006",
+  appId: "1:267641714006:web:66786cf0c1e42cbd32d2f7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+    
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig)
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.database();
